7d58fb1cc9a9f3a4b25c8e16a35f0e3a07723982
尤雨东 <root@ltpp.vip>
2025-11-29 10:24:45 +0800

    feat: v10.6.11

--- debug/src/main.rs
+++ debug/src/main.rs
@@ -1139,7 +1139,7 @@ async fn main() {
     let server_control_hook_2: ServerControlHook = server_control_hook_1.clone();
     tokio::spawn(async move {
         tokio::time::sleep(std::time::Duration::from_secs(60)).await;
-        server_control_hook_1.shutdown().await;
+        server_control_hook_2.shutdown().await;
     });
-    server_control_hook_2.wait().await;
+    server_control_hook_1.wait().await;
 }
